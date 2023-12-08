198b7fc3c523bd71774272ab023d50dea08fe32f
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-12-08 16:35:59 -0300

    Add basic About page

---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <h2>Acerca de Nosotros</h2>
+      <p>Somos PetFashion, expertos en el cuidado y estética de tu mascota.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
