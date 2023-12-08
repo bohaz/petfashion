e5d638cf6e536d4e942c4aeed5778c6a1dab8d0b
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-12-08 16:36:17 -0300

    Add basic contact page

---
+++ src/components/ContactPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ContactPage() {
+  return (
+    <div>
+      <h2>Contacto</h2>
+      <p>¿Tienes preguntas? Contáctanos a través de nuestro formulario.</p>
+      {/* Aquí puedes agregar un formulario de contacto */}
+    </div>
+  );
+}
+
+export default ContactPage;
