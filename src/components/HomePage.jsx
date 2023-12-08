9dea62c58fe10c5c86e8d84c5263b3c5a94ba631
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-12-08 20:22:43 -0300

    Update the home page component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import AboutPage from './AboutPage';
 import ContactPage from './ContactPage';
+import MyFooter from './Footer';
+import ListaDeServicios from './ListaDeServicios';
+import NuestrosTrabajos from './NuestrosTrabajos';
 
 function HomePage() {
   return (
@@ -8,7 +11,10 @@ function HomePage() {
       <h2>Página de Inicio</h2>
       <p>Bienvenido a PetFashion, tu peluquería canina de confianza.</p>
       <AboutPage />
+      <ListaDeServicios />
+      <NuestrosTrabajos />
       <ContactPage />
+      <MyFooter />
     </div>
   );
 }
