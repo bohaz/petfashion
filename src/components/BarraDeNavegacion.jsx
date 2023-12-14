83200ba32c5c9e005ff010be15846a03727c3b2a
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-12-13 21:56:56 -0300

    Change position to fixed

--- src/components/BarraDeNavegacion.jsx
+++ src/components/BarraDeNavegacion.jsx
@@ -66,7 +66,7 @@ export default function SearchAppBar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="fixed">
         <Toolbar>
           <IconButton
             size="large"
