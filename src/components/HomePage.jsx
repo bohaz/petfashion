572bda5e1f83a01741f29af95fa7062cdc14fd61
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-12-15 20:26:59 -0300

    Update Home

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import AboutPage from './AboutPage';
 import ContactPage from './ContactPage';
 import ListaDeServicios from './ListaDeServicios';
@@ -8,17 +8,12 @@ import Trabajos from './Trabajos';
 function HomePage() {
   return (
     <Box>
-            <Typography variant="h3" component="h2" gutterBottom textAlign="center" sx={{ color: '#dd8ea4', fontWeight: '700', fontFamily: 'Poppins, sans-serif', fontSize: { xs: 'h4.fontSize', sm: 'h3.fontSize' } }}>
-      Pet fashion - Peluquería canina
-      </Typography>
-      <Typography variant="h5" sx={{ textAlign: 'center', marginBottom: 4 }}>
-      Especialistas en estética canina
-      </Typography>
 
       <AboutPage />
       <ListaDeServicios />
       <Trabajos />
       <ContactPage />
+      
     </Box>
   );
 }
