6b81076925541165a7d36eab56c55caa91f2bf26
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-12-14 21:23:58 -0300

    Update services

--- src/components/ListaDeServicios.jsx
+++ src/components/ListaDeServicios.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Servicio from "./Servicio";
+import { Grid } from '@mui/material';
+import { Typography } from '@mui/material';
+import { Box } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShieldDog } from '@fortawesome/free-solid-svg-icons';
 
 function ListaDeServicios() {
   const servicios = [
@@ -9,11 +14,26 @@ function ListaDeServicios() {
   ];
 
   return (
-    <div>
-      {servicios.map((servicio, index) => (
-        <Servicio key={index} nombre={servicio.nombre} descripcion={servicio.descripcion} precio={servicio.precio} />
-      ))}
-    </div>
+    <Box sx={{ margin: 3 }}>
+      <Typography variant="h3" textAlign="center" gutterBottom>
+        Nuestros Servicios
+      </Typography>
+      <Box textAlign="center">
+        <FontAwesomeIcon icon={faShieldDog} style={{ fontSize: '50px' }} />
+      </Box>
+
+      <Grid container spacing={2}>
+        {servicios.map((servicio, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Servicio
+              nombre={servicio.nombre}
+              descripcion={servicio.descripcion}
+              precio={servicio.precio}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 }
 
--- src/components/Servicio.jsx
+++ src/components/Servicio.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { Card, CardContent, Typography } from '@mui/material';
 
 function Servicio({ nombre, descripcion, precio }) {
   return (
-    <div>
-      <h2>{nombre}</h2>
-      <p>{descripcion}</p>
-      <p>Precio: ${precio}</p>
-    </div>
+    <Card sx={{ marginBottom: 2 }}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          {nombre}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {descripcion}
+        </Typography>
+        <Typography variant="body2">
+          Precio: ${precio}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
 
