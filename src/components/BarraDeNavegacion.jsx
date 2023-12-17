c1eb60e45661fb25088c057c69a07b1abe7d8039
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-12-17 13:24:08 -0300

    Remove the serach icon

--- src/components/BarraDeNavegacion.jsx
+++ src/components/BarraDeNavegacion.jsx
@@ -1,58 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { styled, alpha } from '@mui/material/styles';
 import { useScrollTrigger } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import InputBase from '@mui/material/InputBase';
 import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
 import Button from '@mui/material/Button';
 import MobileMenu from './MobileMenu';
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  width: '100%',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -125,16 +80,7 @@ export default function SearchAppBar() {
             <Button sx={ linkStyle } component={Link} to="/contact">Contáctanos</Button>
 
           </Box>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon sx={{ color: 'black' }} />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-              sx={{ color: 'black' }}
-            />
-          </Search>
+
         </Toolbar>
       </AppBar>
       </ElevationScroll>
