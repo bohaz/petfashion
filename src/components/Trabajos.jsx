e67f714c3eea9ad4fbbe89a4f4e2227e7e345061
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-01-18 19:40:01 -0300

    Remove label from work component

--- src/components/Trabajos.jsx
+++ src/components/Trabajos.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
@@ -61,43 +60,26 @@ function SwipeableTextMobileStepper() {
 
   return (
     <Box sx={{ pb: '10%', pt: '5%', p: '5%' }}>
-       <Typography
-          variant="h3"
-          component="h2"
-          gutterBottom
-          textAlign="center"
-          sx={{
-            color: '#31383f',
-            fontWeight: '700',
-            fontFamily: 'Poppins, sans-serif',
-            fontSize: { xs: 'h4.fontSize', sm: 'h3.fontSize' }
-          }}
-        >
+      <Typography
+        variant="h3"
+        component="h2"
+        gutterBottom
+        textAlign="center"
+        sx={{
+          color: '#31383f',
+          fontWeight: '700',
+          fontFamily: 'Poppins, sans-serif',
+          fontSize: { xs: 'h4.fontSize', sm: 'h3.fontSize' }
+        }}
+      >
         Nuestros Trabajos
       </Typography>
       <Box textAlign="center" sx={{ marginBottom: 2 }}>
-      <FontAwesomeIcon icon={faPaw} style={{ fontSize: '50px', color: '#dd8ea4'}} />
+        <FontAwesomeIcon icon={faPaw} style={{ fontSize: '50px', color: '#dd8ea4'}} />
       </Box>
       <Slider {...settings}>
         {images.map((image, index) => (
           <Box key={index} sx={{ textAlign: 'center' }}>
-            {image.label && (
-              <Paper
-                square
-                elevation={3}
-                sx={{
-                  margin: 'auto',
-                  marginBottom: 2,
-                  padding: 1,
-                  maxWidth: '50%',
-                  borderRadius: '30px',
-                  backgroundColor: '#fedd89',
-                  color: '#31383f',
-                }}
-              >
-                <Typography variant="h6" sx={{ fontWeight: 'bold' }}>{image.label}</Typography>
-              </Paper>
-            )}
             <Box
               component="img"
               sx={{
@@ -108,7 +90,7 @@ function SwipeableTextMobileStepper() {
                 borderRadius: 2,
               }}
               src={image.imgPath}
-              alt={image.label}
+              alt=""
             />
           </Box>
         ))}
