7424d363e23e79cc135b39246b09501e69ab7c16
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-02-02 16:07:37 -0300

    New opinion component

---
+++ src/components/TrustmaryWidget.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+
+const TrustmaryWidget = () => {
+  useEffect(() => {
+    const scriptId = 'trustmary-widget-script';
+    if (document.getElementById(scriptId)) return;
+  
+    setTimeout(() => {
+      const script = document.createElement('script');
+      script.id = scriptId;
+      script.src = "https://widget.trustmary.com/3Ss79-l0j";
+      script.async = true;
+  
+      const container = document.getElementById("trustmary-widget-container");
+      if (container) {
+        container.appendChild(script);
+      }
+    }, 1000); // Retrasa la ejecución en 1 segundo
+  
+    return () => {
+      const existingScript = document.getElementById(scriptId);
+      if (existingScript) {
+        existingScript.remove();
+      }
+    };
+  }, []);
+  
+
+  // Este div actúa como el contenedor para el widget
+  return <div id="trustmary-widget-container"></div>;
+};
+
+export default TrustmaryWidget;
