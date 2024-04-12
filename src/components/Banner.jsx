88162eaa8c232ebbbb69a7bc13d18cf8c6e95caa
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-04-12 14:51:10 -0400

    Add booking button

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import logo from '../assets/PfLogo.png';
+import logo from '../assets/PfLogo2.png';
+import BookingButton from './BookingButton';
 
 function Banner() {
   const fadeIn = {
@@ -28,12 +29,14 @@ function Banner() {
       >
         <img src={logo} alt="Logo de la empresa" style={{ height: '250px', margin: '0 auto' }} />
       </motion.div>
+      <BookingButton />
+
       <Typography
         variant="h3"
         component="h2"
         gutterBottom
         sx={{
-          color: '#dd8ea4', fontWeight: '700', fontFamily: 'Poppins, sans-serif', fontSize: { xs: 'h5.fontSize', sm: 'h3.fontSize' },
+          color: '#dd8ea4', fontWeight: '700', fontFamily: 'Poppins, sans-serif', mt: '20px', fontSize: { xs: 'h5.fontSize', sm: 'h3.fontSize' },
         }}
       >
         Pet fashion - Peluquería canina
