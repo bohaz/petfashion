cfe1d7322e98b037a38702b3102b63aaeb301bf9
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-14 14:06:26 -0400

    Create new componnent

---
+++ src/components/Visit.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
+function Visit() {
+  const [isVisible, setIsVisible] = useState(false);
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+  });
+
+  if (inView) {
+    setIsVisible(true);
+  }
+
+  return (
+    <div className="flex flex-col text-center font-bold p-2" ref={ref}>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: isVisible ? 1 : 0, y: isVisible ? 0 : 20 }}
+        transition={{ duration: 0.5 }}
+      >
+        <h1>Visítanos</h1>
+        <p>Lunes - Sábado: 9:00 - 18:00</p>
+        <p>Domingo: Cerrado</p>
+        <p>Estamos ubicados en la Calle Santa Luisa #51, Isla Negra</p>
+      </motion.div>
+    </div>
+  );
+}
+
+export default Visit;
