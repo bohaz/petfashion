0ac2eba074779388f61913c1b4ebbf8df1bb7c76
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-14 15:58:56 -0400

    Add refactored component

--- src/components/Instagram.jsx
+++ src/components/Instagram.jsx
@@ -1,72 +1,40 @@
 import React from 'react';
-import { Box } from '@mui/material';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 import background from '../assets/GroomingTools2.webp';
 
 function Instagram() {
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+    threshold: 0.5,
+  });
+
   return (
-    <Box sx={{
-      position: 'relative',
-      '&::before': {
-        content: '""',
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        backgroundImage: `url(${background})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        opacity: 0.6,
-        zIndex: -1,
-      },
-      width: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      pb: '12%',
-      pt: '12%',
-    }}
-    >
-      <Box
-        sx={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          backgroundColor: 'rgba(0, 0, 0, 0.7)',
-          zIndex: -1,
-        }}
-      />
-      <Typography
-        variant="h3"
-        component="h2"
-        gutterBottom
-        textAlign="center"
-        sx={{
-          color: 'white',
-          fontWeight: '700',
-          fontFamily: 'Poppins, sans-serif',
-          fontSize: { xs: 'h5.fontSize', sm: 'h3.fontSize' },
-        }}
+    <div className="relative flex flex-col justify-center text-center font-bold p-3 h-96 text-white tracking-wide" style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'center' }} ref={ref}>
+      <div className="absolute inset-0 bg-neutral-800 opacity-80" />
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+        transition={{ duration: 0.5 }}
+        className="relative z-10"
       >
-        PetFashion está en Instagram
-      </Typography>
-      <Typography variant="h5" sx={{ textAlign: 'center', color: 'white', fontSize: { xs: 'h6.fontSize', sm: 'h5.fontSize' } }}>
-        Siguenos
-        {' '}
-        <Link
-          href="https://www.instagram.com/pet_fashion17?igsh=MWx3eHVvYThrdm9nMw=="
-          target="_blank"
-          rel="noopener noreferrer"
-          sx={{ color: 'white', textDecoration: 'none', '&:hover': { textDecoration: 'underline' } }}
-        >
-          @pet_fashion17
-        </Link>
-      </Typography>
-    </Box>
+        <h1 className="text-2xl md:text-5xl mb-2">PetFashion está en Instagram</h1>
+        <p className="text-xl md:text-3xl">
+          Siguenos
+          {' '}
+          <a
+            href="https://www.instagram.com/pet_fashion17?igsh=MWx3eHVvYThrdm9nMw=="
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-fuchsia-400 underline hover:text-fuchsia-500"
+          >
+            @pet_fashion17
+          </a>
+        </p>
+
+      </motion.div>
+    </div>
+
   );
 }
 
