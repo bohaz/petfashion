d1245b43d9d7266e1cff24f74e2d0c39ce4df904
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-14 15:09:30 -0400

    Add the booking button

--- src/components/Visit.jsx
+++ src/components/Visit.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
+import background from '../assets/GroomingTools.webp';
+import BookingButton from './BookingButton';
 
 function Visit() {
   const { ref, inView } = useInView({
@@ -9,18 +11,22 @@ function Visit() {
   });
 
   return (
-    <div className="flex flex-col text-center font-bold p-2" ref={ref}>
+    <div className="relative flex flex-col justify-center text-center font-bold p-3 h-72 text-white" style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'center' }} ref={ref}>
+      <div className="absolute inset-0 bg-neutral-800 opacity-80" />
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
         transition={{ duration: 0.5 }}
+        className="relative z-10"
       >
-        <h1>Visítanos</h1>
-        <p>Lunes - Sábado: 9:00 - 18:00</p>
-        <p>Domingo: Cerrado</p>
-        <p>Estamos ubicados en la Calle Santa Luisa #51, Isla Negra</p>
+        <h1 className="text-3xl mb-2">Visítanos</h1>
+        <p className="text-xl">Lunes - Sábado: 9:00 - 18:00</p>
+        <p className="text-xl">Domingo: Cerrado</p>
+        <p className="text-xl">Estamos ubicados en la Calle Santa Luisa #51, Isla Negra</p>
+        <BookingButton />
       </motion.div>
     </div>
+
   );
 }
 
