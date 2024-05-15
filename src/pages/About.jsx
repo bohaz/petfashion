7946f35a7a4ee26385ea8c8cee174cde336af119
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-15 11:21:59 -0400

    Add some classes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,33 @@
+import React from 'react';
+import HuellaCorazon from '../assets/HuellaCorazon.png';
+import HuellaCruz from '../assets/HuellaCruz.png';
+import MedallaHuella from '../assets/MedallaHuella.png';
+
 function About() {
   return (
-    <div>
-      <h1>Quiénes Somos</h1>
-      <p>This is the about page.</p>
+    <div className="p-4 flex flex-col gap-5">
+      <h1 className="font-bold text-2xl text-center">Quiénes Somos</h1>
+      <div className="flex flex-col items-center">
+        <img className="w-52" src={MedallaHuella} alt="Huella" />
+        <p className="bg-amber-200 p-3 rounded-lg shadow-lg shadow-neutral-400 text-center">
+          Somos una empresa familiar dedicada a prestar el servicio
+          de peluquería canina que tu mascota necesita.
+        </p>
+      </div>
+      <div className="flex flex-col items-center">
+        <img className="w-52" src={HuellaCruz} alt="Huella" />
+        <p className="bg-amber-200 p-3 rounded-lg shadow-lg shadow-neutral-400 text-center">
+          Nuestro equipo está conformado por un médico veterinario y una excelente
+          estilista canina trabajando para usted.
+        </p>
+      </div>
+      <div className="flex flex-col items-center">
+        <img className="w-52" src={HuellaCorazon} alt="Huella" />
+        <p className="bg-amber-200 p-3 rounded-lg shadow-lg shadow-neutral-400 text-center">
+          No usamos bozal ni sedación. La idea principal es que tu mascota
+          se sienta cómoda y segura.
+        </p>
+      </div>
     </div>
   );
 }
