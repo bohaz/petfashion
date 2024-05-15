87c76f7323dd214cbcdc27b4d94101ff5a7ddda3
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-15 15:25:02 -0400

    Add desktop styles

--- src/pages/Servicios.jsx
+++ src/pages/Servicios.jsx
@@ -21,24 +21,24 @@ function Servicios() {
           <table className="table-auto border-collapse">
             <thead>
               <tr>
-                <th className="text-center p-2 bg-pink-300" colSpan="2">Servicio de Corte y Baño</th>
+                <th className="text-center md:text-lg p-2 bg-pink-300" colSpan="2">Servicio de Corte y Baño</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td className="bg-neutral-100 p-2">0kg hasta 5kg</td>
-                <td className="bg-neutral-100 p-2">$20.000</td>
+                <td className="bg-neutral-100 p-2 md:text-lg">0kg hasta 5kg</td>
+                <td className="bg-neutral-100 p-2 md:text-lg">$20.000</td>
               </tr>
               <tr>
-                <td className="p-2">5,1kg hasta 10kg</td>
-                <td className="p-2">$25.000</td>
+                <td className="p-2 md:text-lg">5,1kg hasta 10kg</td>
+                <td className="p-2 md:text-lg">$25.000</td>
               </tr>
               <tr>
-                <td className="bg-neutral-100 p-2">10,1kg hasta 20kg</td>
-                <td className="bg-neutral-100 p-2">$30.000</td>
+                <td className="bg-neutral-100 p-2 md:text-lg">10,1kg hasta 20kg</td>
+                <td className="bg-neutral-100 p-2 md:text-lg">$30.000</td>
               </tr>
               <tr>
-                <td className="bg-neutral-300 text-center font p-2 italic" colSpan="2">Los valores varían de acuerdo a la condición del pelaje de la mascota.</td>
+                <td className="bg-neutral-300 text-center font p-2 italic md:text-lg" colSpan="2">Los valores varían de acuerdo a la condición del pelaje de la mascota.</td>
               </tr>
             </tbody>
           </table>
@@ -46,24 +46,24 @@ function Servicios() {
           <table className="table-auto text-center border-collapse">
             <thead>
               <tr>
-                <th className="text-center bg-pink-300 p-2" colSpan="2">Servicio Solo Baño</th>
+                <th className="text-center bg-pink-300 p-2 md:text-lg" colSpan="2">Servicio Solo Baño</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td className="bg-neutral-100 p-2">0kg hasta 5kg</td>
-                <td className="bg-neutral-100 p-2">$15.000</td>
+                <td className="bg-neutral-100 p-2 md:text-lg">0kg hasta 5kg</td>
+                <td className="bg-neutral-100 p-2 md:text-lg">$15.000</td>
               </tr>
               <tr>
-                <td className="p-2">5,1kg hasta 10kg</td>
-                <td className="p-2">$20.000</td>
+                <td className="p-2 md:text-lg">5,1kg hasta 10kg</td>
+                <td className="p-2 md:text-lg">$20.000</td>
               </tr>
               <tr>
-                <td className="bg-neutral-100 p-2">10,1kg hasta 20kg</td>
-                <td className="bg-neutral-100 p-2">$25.000</td>
+                <td className="bg-neutral-100 p-2 md:text-lg">10,1kg hasta 20kg</td>
+                <td className="bg-neutral-100 p-2 md:text-lg">$25.000</td>
               </tr>
               <tr>
-                <td className="bg-neutral-300 text-center p-2 italic" colSpan="2">Los valores varían de acuerdo a la condición del pelaje de la mascota.</td>
+                <td className="bg-neutral-300 text-center p-2 italic md:text-lg" colSpan="2">Los valores varían de acuerdo a la condición del pelaje de la mascota.</td>
               </tr>
             </tbody>
           </table>
