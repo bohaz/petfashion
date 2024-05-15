cd7abb68a28dd4199b5c23536030b7b9f3216b63
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-15 12:45:09 -0400

    Add descktop styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,7 +14,7 @@ function About() {
   });
   return (
     <div
-      className="p-4 pt-10 pb-10 flex flex-col gap-5"
+      className="p-4 pt-10 pb-10 flex flex-col gap-5 items-center"
       style={{
         backgroundImage: `url(${Huesos})`,
         backgroundAttachment: 'fixed',
@@ -27,24 +27,24 @@ function About() {
         transition={{ duration: 0.5 }}
         className="relative z-10"
       >
-        <h1 className="font-bold text-4xl text-center text-neutral-800">Quiénes Somos</h1>
-        <div className="flex flex-col items-center">
+        <h1 className="font-bold mb-5 md:mb-10 text-4xl md:text-5xl xl:text-6xl text-center text-neutral-700">Quiénes Somos</h1>
+        <div className="flex flex-col items-center md:flex-row">
           <img className="w-52" src={MedallaHuella} alt="Huella" />
-          <p className="bg-amber-200 p-3 rounded-lg shadow-lg shadow-neutral-400 text-center">
+          <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg shadow-neutral-400 text-center max-w-lg">
             Somos una empresa familiar dedicada a prestar el servicio
             de peluquería canina que tu mascota necesita.
           </p>
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center md:flex-row-reverse">
           <img className="w-52" src={HuellaCruz} alt="Huella" />
-          <p className="bg-amber-200 p-3 rounded-lg shadow-lg shadow-neutral-400 text-center">
+          <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg shadow-neutral-400 text-center max-w-lg">
             Nuestro equipo está conformado por un médico veterinario y una excelente
             estilista canina trabajando para usted.
           </p>
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center md:flex-row">
           <img className="w-52" src={HuellaCorazon} alt="Huella" />
-          <p className="bg-amber-200 p-3 rounded-lg shadow-lg shadow-neutral-400 text-center">
+          <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg shadow-neutral-400 text-center max-w-lg">
             No usamos bozal ni sedación. La idea principal es que tu mascota
             se sienta cómoda y segura.
           </p>
