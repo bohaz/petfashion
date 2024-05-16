db8a56e503a0b0c643b0c278cb55c75a73cd6037
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-16 12:38:34 -0400

    Add card component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,7 @@ import HuellaCorazon from '../assets/Icono1.png';
 import HuellaCruz from '../assets/Icono2.png';
 import MedallaHuella from '../assets/Icono3.png';
 import VideoButton from '../components/VideoButton';
+import Cards from '../components/Cards';
 
 function About() {
   const { ref, inView } = useInView({
@@ -13,10 +14,7 @@ function About() {
   });
   return (
     <div
-      className="p-4 pt-10 pb-10 flex flex-col gap-5 items-center bg-contain bg-center bg-pink-100"
-      style={{
-
-      }}
+      className="p-4 pt-10 flex flex-col gap-5 items-center bg-contain bg-center bg-pink-100"
       ref={ref}
     >
       <motion.div
@@ -28,26 +26,27 @@ function About() {
         <h1 className="font-bold mb-5 md:mb-10 text-4xl md:text-5xl xl:text-6xl text-center text-gray-800">Quiénes Somos</h1>
         <div className="flex flex-col items-center md:flex-row">
           <img className="w-52" src={MedallaHuella} alt="Huella" />
-          <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg text-center max-w-lg text-gray-800">
+          <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg text-center max-w-lg text-gray-800 border-4 border-amber-400">
             Somos una empresa familiar dedicada a prestar el servicio
             de peluquería canina que tu mascota necesita.
           </p>
         </div>
         <div className="flex flex-col items-center md:flex-row-reverse">
           <img className="w-52" src={HuellaCruz} alt="Huella" />
-          <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg text-center max-w-lg text-gray-800">
+          <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg text-center max-w-lg text-gray-800 border-4 border-amber-400">
             Nuestro equipo está conformado por un médico veterinario y una excelente
             estilista canina trabajando para usted.
           </p>
         </div>
         <div className="flex flex-col items-center md:flex-row">
           <img className="w-52" src={HuellaCorazon} alt="Huella" />
-          <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg text-center max-w-lg text-gray-800">
+          <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg text-center max-w-lg text-gray-800 border-4 border-amber-400">
             No usamos bozal ni sedación. La idea principal es que tu mascota
             se sienta cómoda y segura.
           </p>
         </div>
         <VideoButton videoId="6EK9DO9iZmw" />
+        <Cards />
       </motion.div>
     </div>
   );
