172c122004ab7fa96197dc3e6e98515e79c715e4
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-16 10:08:53 -0400

    Remove shadow color

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -28,21 +28,21 @@ function About() {
         <h1 className="font-bold mb-5 md:mb-10 text-4xl md:text-5xl xl:text-6xl text-center text-gray-800">Quiénes Somos</h1>
         <div className="flex flex-col items-center md:flex-row">
           <img className="w-52" src={MedallaHuella} alt="Huella" />
-          <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg shadow-pink-200 text-center max-w-lg text-gray-800">
+          <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg text-center max-w-lg text-gray-800">
             Somos una empresa familiar dedicada a prestar el servicio
             de peluquería canina que tu mascota necesita.
           </p>
         </div>
         <div className="flex flex-col items-center md:flex-row-reverse">
           <img className="w-52" src={HuellaCruz} alt="Huella" />
-          <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg shadow-pink-200 text-center max-w-lg text-gray-800">
+          <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg text-center max-w-lg text-gray-800">
             Nuestro equipo está conformado por un médico veterinario y una excelente
             estilista canina trabajando para usted.
           </p>
         </div>
         <div className="flex flex-col items-center md:flex-row">
           <img className="w-52" src={HuellaCorazon} alt="Huella" />
-          <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg shadow-pink-200 text-center max-w-lg text-gray-800">
+          <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg text-center max-w-lg text-gray-800">
             No usamos bozal ni sedación. La idea principal es que tu mascota
             se sienta cómoda y segura.
           </p>
