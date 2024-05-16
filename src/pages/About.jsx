657ab96bf355d47d856b297d61374e891aae4d6b
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-16 16:45:11 -0400

    Add icon

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,26 +4,34 @@ import { useInView } from 'react-intersection-observer';
 import HuellaCorazon from '../assets/Icono1.png';
 import HuellaCruz from '../assets/Icono2.png';
 import MedallaHuella from '../assets/Icono3.png';
-import VideoButton from '../components/VideoButton';
-import Cards from '../components/Cards';
+/* import VideoButton from '../components/VideoButton'; */
+/* import Cards from '../components/Cards'; */
 
 function About() {
   const { ref, inView } = useInView({
     triggerOnce: false,
-    threshold: 0.5,
+    threshold: 0.3,
   });
+
   return (
     <div
-      className="p-4 pt-10 flex flex-col gap-5 items-center bg-contain bg-center bg-pink-100"
+      className="p-4 pb-10 pt-10 flex flex-col gap-5 items-center bg-contain bg-center bg-pink-100"
       ref={ref}
     >
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
-        transition={{ duration: 0.5 }}
+        transition={{ duration: 0.5, delay: 0.1 }}
         className="relative z-10"
       >
         <h1 className="font-bold mb-5 md:mb-10 text-4xl md:text-5xl xl:text-6xl text-center text-gray-800">Quiénes Somos</h1>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: 20 }}
+        animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : 20 }}
+        transition={{ duration: 0.5, delay: 0.1 }}
+        className="relative z-10"
+      >
         <div className="flex flex-col items-center md:flex-row">
           <img className="w-52" src={MedallaHuella} alt="Huella" />
           <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg text-center max-w-lg text-gray-800 border-4 border-amber-400">
@@ -31,6 +39,13 @@ function About() {
             de peluquería canina que tu mascota necesita.
           </p>
         </div>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: -20 }}
+        animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : -20 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        className="relative z-10"
+      >
         <div className="flex flex-col items-center md:flex-row-reverse">
           <img className="w-52" src={HuellaCruz} alt="Huella" />
           <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg text-center max-w-lg text-gray-800 border-4 border-amber-400">
@@ -38,6 +53,13 @@ function About() {
             estilista canina trabajando para usted.
           </p>
         </div>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: 20 }}
+        animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : 20 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="relative z-10"
+      >
         <div className="flex flex-col items-center md:flex-row">
           <img className="w-52" src={HuellaCorazon} alt="Huella" />
           <p className="bg-amber-200 p-4 md:text-lg rounded-lg shadow-lg text-center max-w-lg text-gray-800 border-4 border-amber-400">
@@ -45,9 +67,11 @@ function About() {
             se sienta cómoda y segura.
           </p>
         </div>
-        <VideoButton videoId="6EK9DO9iZmw" />
-        <Cards />
       </motion.div>
+      {/* <VideoButton videoId="6EK9DO9iZmw" /> */}
+      <div>
+        {/* <Cards /> */}
+      </div>
     </div>
   );
 }
