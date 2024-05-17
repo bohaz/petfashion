0b6c8caf48fc2ddebbf5be42941371adccd12ee9
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-17 13:03:10 -0400

    Add delay

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,6 +5,7 @@ import Card1 from '../assets/Icono5.png';
 import Card2 from '../assets/Icono9.png';
 /* import Card3 from '../assets/Icono7.png'; */
 import Card4 from '../assets/Icono8.png';
+import Card5 from '../assets/arrow.svg';
 
 function Cards() {
   const { ref, inView } = useInView({
@@ -17,31 +18,51 @@ function Cards() {
       className="p-4 pb-10 pt-10 flex flex-col gap-5 items-center bg-contain bg-center bg-pink-100"
       ref={ref}
     >
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
-        transition={{ duration: 0.5, delay: 0.1 }}
-        className="relative z-10"
-      >
-        <div className="flex flex-col gap-5 items-center md:flex-row">
+
+      <div className="flex flex-col gap-5 items-center md:flex-row">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+          transition={{ duration: 0.5, delay: 0.1 }}
+          className="relative z-10"
+        >
           <div className="bg-amber-200 rounded-xl p-4 border-4 border-amber-400 shadow-xl">
             <img className="w-60" src={Card4} alt="icono" />
+            <img className="w-10 animate-bounce m-auto mt-2" src={Card5} alt="icono" />
             <h2 className="text-center text-gray-800">Agenda de horas</h2>
           </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="relative z-10"
+        >
           <div className="bg-amber-200 rounded-xl p-4 border-4 border-amber-400 shadow-xl">
             <img className="w-60" src={Card1} alt="icono" />
+            <img className="w-10 animate-bounce m-auto mt-2" src={Card5} alt="icono" />
             <h2 className="text-center text-gray-800">Corte y Baño</h2>
           </div>
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          className="relative z-10"
+        >
           <div className="bg-amber-200 rounded-xl p-4 border-4 border-amber-400 shadow-xl">
             <img className="w-60" src={Card2} alt="icono" />
+            <img className="w-10 animate-bounce m-auto mt-2" src={Card5} alt="icono" />
             <h2 className="text-center text-gray-800">Servicio de Baño</h2>
           </div>
-          {/* <div className="bg-amber-200 rounded-xl  p-4 border-4 border-amber-400 shadow-xl">
+        </motion.div>
+        {/* <div className="bg-amber-200 rounded-xl  p-4 border-4 border-amber-400 shadow-xl">
             <img className="w-52" src={Card3} alt="icono" />
             <h2 className="text-center text-gray-800">Corte de </h2>
           </div> */}
-        </div>
-      </motion.div>
+      </div>
+
     </div>
 
   );
