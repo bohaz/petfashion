16da494882b183b3359fe177c873f1ccae7f86fa
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-20 14:06:07 -0400

    Add motion

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,25 +1,63 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
 function Booking() {
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+    threshold: 0.3,
+  });
+
   return (
     <div
       className="p-4 pb-10 pt-10 flex flex-col gap-5 items-center bg-contain bg-center bg-pink-100"
       ref={ref}
     >
       <div>
-        <h1>Antes de Agendar</h1>
-        <p>
-          No usamos bozal ni sedación.
-          La idea principal es brindar BIENESTAR,
-          para lo cual la mascota debe tolerar la manipulación.
-        </p>
-        <p>
-          No recibimos mascotas con pulgas y/o garrapatas.
-          La mascota debe ponerse al día con su comprimido o pipeta antiparasitaria.
-        </p>
-        <p>
-          No rapamos mascotas.
-          Nos enfocamos en prestar un servicio saludable acorde a la raza.
-        </p>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+          transition={{ duration: 0.5, delay: 0.1 }}
+          className="relative z-10"
+        >
+          <h1 className="font-bold mb-5 md:mb-10 text-4xl md:text-5xl xl:text-6xl text-center text-gray-800">Antes de Agendar</h1>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="relative z-10"
+        >
+          <p>
+            No usamos bozal ni sedación.
+            La idea principal es brindar BIENESTAR,
+            para lo cual la mascota debe tolerar la manipulación.
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          className="relative z-10"
+        >
+          <p>
+            No recibimos mascotas con pulgas y/o garrapatas.
+            La mascota debe ponerse al día con su comprimido o pipeta antiparasitaria.
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+          transition={{ duration: 0.5, delay: 0.4 }}
+          className="relative z-10"
+        >
+          <p>
+            No rapamos mascotas.
+            Nos enfocamos en prestar un servicio saludable acorde a la raza.
+          </p>
+        </motion.div>
       </div>
+
     </div>
   );
 }
