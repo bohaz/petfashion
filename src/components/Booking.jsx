28c7aacf51764aeb8ff609e5e61c85469169490f
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-20 14:00:30 -0400

    Add a parrent div for motion

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,7 +1,25 @@
 function Booking() {
   return (
-    <div>
-      <h1>Booking</h1>
+    <div
+      className="p-4 pb-10 pt-10 flex flex-col gap-5 items-center bg-contain bg-center bg-pink-100"
+      ref={ref}
+    >
+      <div>
+        <h1>Antes de Agendar</h1>
+        <p>
+          No usamos bozal ni sedación.
+          La idea principal es brindar BIENESTAR,
+          para lo cual la mascota debe tolerar la manipulación.
+        </p>
+        <p>
+          No recibimos mascotas con pulgas y/o garrapatas.
+          La mascota debe ponerse al día con su comprimido o pipeta antiparasitaria.
+        </p>
+        <p>
+          No rapamos mascotas.
+          Nos enfocamos en prestar un servicio saludable acorde a la raza.
+        </p>
+      </div>
     </div>
   );
 }
