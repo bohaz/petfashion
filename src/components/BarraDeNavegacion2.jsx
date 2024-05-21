568f2a5f99a355269d863a0392eaa31d1c400f0a
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-21 13:19:58 -0400

    add links and styles

--- src/components/BarraDeNavegacion2.jsx
+++ src/components/BarraDeNavegacion2.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../assets/PfLogo2.png';
+import Menu from '../assets/svg icons/menu.svg';
 
 function BarraDeNavegacion2() {
   return (
-    <nav>
-      <ul>
+    <nav className="p-2">
+      <ul className="flex items-center justify-between">
         <li>
+          <img className="w-24" src={Logo} alt="Logo" />
+        </li>
+        <li className="hidden md:block">
           <Link to="/inicio">Inicio</Link>
         </li>
-        <li>
-          <Link to="/inicio">Inicio</Link>
-        </li>
-        <li>
+        <li className="hidden md:block">
           <Link to="/acerca">Acerca</Link>
         </li>
-        <li>
+        <li className="hidden md:block">
           <Link to="/contacto">Contacto</Link>
+        </li>
+        <li className="block md:hidden">
+          <img className="w-12" src={Menu} alt="Menu" />
         </li>
       </ul>
     </nav>
