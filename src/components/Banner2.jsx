30040fdaeef68aaf3950bc2fdb38ed42a32d9ab4
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-21 12:25:52 -0400

    Add typography styles

--- src/components/Banner2.jsx
+++ src/components/Banner2.jsx
@@ -22,11 +22,11 @@ function Banner2() {
     cssEase: 'linear',
   };
   return (
-    <div className="relative">
+    <div className="relative mt-16 pt-2">
       <div className="absolute inset-0 bg-neutral-800 opacity-70 z-10" />
       <div className="absolute inset-0 flex flex-col items-center justify-center z-20 text-white">
         <h1 className="text-2xl md:text-6xl font-bold text-pink-400">Pet fashion - Peluquería canina</h1>
-        <p className="text-2xl md:text-4xl">Especialistas en estética canina</p>
+        <p className="text-xl md:text-4xl">Especialistas en estética canina</p>
       </div>
       <Slider {...settings}>
         <div className="flex items-stretch">
