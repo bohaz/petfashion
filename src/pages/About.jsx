b025b66347ca6b01d080a4ceab09e6ac06de859b
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-21 16:07:00 -0400

    Adjust margin bottom

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,7 +17,7 @@ function About() {
       ref={ref}
     >
 
-      <h1 className="font-bold mb-5 md:mb-10 text-4xl md:text-5xl xl:text-6xl text-center text-gray-800 relative after:content-[''] after:block after:w-40 after:h-1 after:bg-pink-400 after:mt-2 after:mx-auto">Quiénes Somos</h1>
+      <h1 className="font-bold md:mb-5 text-4xl md:text-5xl xl:text-6xl text-center text-gray-800 relative after:content-[''] after:block after:w-40 after:h-1 after:bg-pink-400 after:mt-2 after:mx-auto">Quiénes Somos</h1>
 
       <motion.div
         initial={{ opacity: 0, x: 20 }}
