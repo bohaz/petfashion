a57895e5cb3dcc74fed6df4a762221c2a2bd37e2
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-21 14:08:11 -0400

    Add menu and user icons

--- src/components/BarraDeNavegacion2.jsx
+++ src/components/BarraDeNavegacion2.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import IconButton from '@mui/material/IconButton';
 import Logo from '../assets/PfLogo2.png';
 import Menu from '../assets/svg icons/menu.svg';
+import MobileMenu from './MobileMenu';
+import UserIcon from './UserIcon';
 
 function BarraDeNavegacion2() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const handleMobileMenuOpen = () => {
+    setMobileMenuOpen(true);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMenuOpen(false);
+  };
   return (
     <div className="p-2 fixed top-0 left-0 right-0 z-50 bg-white shadow-md">
       <nav>
@@ -23,11 +35,21 @@ function BarraDeNavegacion2() {
           <li className="hidden md:block">
             <Link to="/contact">Contáctanos</Link>
           </li>
+          <li className="ml-60">
+            <IconButton
+              sx={{
+                color: '#dd8ea4', pt: '0',
+              }}
+            >
+              <UserIcon />
+            </IconButton>
+          </li>
           <li className="block md:hidden">
-            <img className="w-12" src={Menu} alt="Menu" />
+            <button type="button" onClick={handleMobileMenuOpen}><img className="w-10" src={Menu} alt="Menu" /></button>
           </li>
         </ul>
       </nav>
+      <MobileMenu open={mobileMenuOpen} onClose={handleMobileMenuClose} />
     </div>
   );
 }
