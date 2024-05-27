be9be269dba739a6c10452f5483a54ba3f0de129
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-27 08:42:04 -0400

    Remove repeated classes

--- src/components/BarraDeNavegacion2.jsx
+++ src/components/BarraDeNavegacion2.jsx
@@ -30,54 +30,56 @@ function BarraDeNavegacion2() {
           <li>
             <img className="w-24 lg:w-28" src={Logo} alt="Logo" />
           </li>
-          <li className="hidden md:block text-pink-400 font-bold text-2xl">
-            <button
-              type="button"
-              onClick={() => handleLinkClick('/')}
-              className="text-pink-400 font-bold text-2xl"
-            >
-              Home
-            </button>
-          </li>
-          <li className="hidden md:block text-pink-400 font-bold text-2xl">
-            <button
-              type="button"
-              onClick={() => handleLinkClick('/about')}
-              className="text-pink-400 font-bold text-2xl"
-            >
-              Acerca
-            </button>
-          </li>
-          <li className="hidden md:block text-pink-400 font-bold text-2xl">
-            <button
-              type="button"
-              onClick={() => handleLinkClick('/servicios')}
-              className="text-pink-400 font-bold text-2xl"
-            >
-              Servicios
-            </button>
-          </li>
-          <li className="hidden md:block text-pink-400 font-bold text-2xl">
-            <button
-              type="button"
-              onClick={() => handleLinkClick('/contact')}
-              className="text-pink-400 font-bold text-2xl"
-            >
-              Contáctanos
-            </button>
-          </li>
-          <li className="ml-60">
-            <IconButton
-              sx={{
-                color: '#dd8ea4', pt: '0',
-              }}
-            >
-              <UserIcon />
-            </IconButton>
-          </li>
-          <li className="block md:hidden">
-            <button type="button" onClick={handleMobileMenuOpen}><img className="w-10" src={Menu} alt="Menu" /></button>
-          </li>
+          <div className="flex gap-4 items-center">
+            <li className="hidden md:block text-pink-400">
+              <button
+                type="button"
+                onClick={() => handleLinkClick('/')}
+                className="text-pink-400 text-2xl"
+              >
+                Home
+              </button>
+            </li>
+            <li className="hidden md:block text-pink-400">
+              <button
+                type="button"
+                onClick={() => handleLinkClick('/about')}
+                className="text-pink-400 text-2xl"
+              >
+                Acerca
+              </button>
+            </li>
+            <li className="hidden md:block text-pink-400">
+              <button
+                type="button"
+                onClick={() => handleLinkClick('/servicios')}
+                className="text-pink-400 text-2xl"
+              >
+                Servicios
+              </button>
+            </li>
+            <li className="hidden md:block text-pink-400">
+              <button
+                type="button"
+                onClick={() => handleLinkClick('/contact')}
+                className="text-pink-400 text-2xl"
+              >
+                Contáctanos
+              </button>
+            </li>
+            <li className="">
+              <IconButton
+                sx={{
+                  color: '#dd8ea4',
+                }}
+              >
+                <UserIcon />
+              </IconButton>
+            </li>
+            <li className="block md:hidden">
+              <button type="button" onClick={handleMobileMenuOpen}><img className="w-10" src={Menu} alt="Menu" /></button>
+            </li>
+          </div>
         </ul>
       </nav>
       <MobileMenu open={mobileMenuOpen} onClose={handleMobileMenuClose} />
