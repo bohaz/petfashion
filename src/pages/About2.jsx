5ba8e94e0492d051ef4496288c0af02f111fcaf8
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-27 10:02:02 -0400

    Adjust colors

--- src/pages/About2.jsx
+++ src/pages/About2.jsx
@@ -14,14 +14,14 @@ function About2() {
 
   return (
     <div
-      className="p-4 pb-10 pt-10 flex flex-col gap-5 items-center bg-contain bg-center bg-orange-100"
+      className="p-4 pb-10 pt-10 flex flex-col gap-5 items-center bg-contain bg-center bg-cream"
       ref={ref}
     >
 
-      <h1 className="font-bold md:mb-20 text-4xl md:text-5xl xl:text-6xl text-center text-gray-800">
+      <h1 className="font-bold md:mb-20 text-4xl md:text-5xl xl:text-6xl text-center text-grayDarker">
         Quiénes
         {' '}
-        <span className="font-shadows text-teal-600">Somos</span>
+        <span className="font-shadows text-tealDark">Somos</span>
       </h1>
 
       <motion.div
@@ -33,14 +33,14 @@ function About2() {
         <div className="flex flex-col items-center md:flex-row md:mb-20 md:-ml-72">
           <img className="w-44 md:w-52" src={MedallaHuella} alt="Huella" />
           <div>
-            <h2 className="font-bold md:mb-5 text-3xl md:text-4xl xl:text-5xl text-gray-800">
+            <h2 className="font-bold md:mb-5 text-3xl md:text-4xl xl:text-5xl text-grayDarker">
               Somos una empresa
               {' '}
-              <span className="font-shadows text-teal-600">familiar</span>
+              <span className="font-shadows text-tealDark">familiar</span>
             </h2>
-            <div className="relative p-6 md:text-lg rounded-xl text-center border-2 border-gray-300 my-8">
+            <div className="relative p-6 md:text-lg rounded-xl text-center border-2 border-grayLight my-8">
               <img className="absolute -top-4 left-6 w-10 bg-orange-100" src={Quotes} alt="Quotes" />
-              <p className="italic text-gray-600">
+              <p className="italic text-grayDark">
                 Dedicada a prestar el servicio
                 de peluquería canina que tu mascota necesita.
               </p>
@@ -58,14 +58,14 @@ function About2() {
         <div className="flex flex-col items-center md:flex-row-reverse md:mb-20 md:-mr-96">
           <img className="w-52 md:w-60" src={HuellaCruz} alt="Huella" />
           <div>
-            <h2 className="font-bold md:mb-5 text-3xl md:text-4xl xl:text-5xl text-gray-800">
+            <h2 className="font-bold md:mb-5 text-3xl md:text-4xl xl:text-5xl text-grayDarker">
               Nuestro
               {' '}
-              <span className="font-shadows text-teal-600">Equipo</span>
+              <span className="font-shadows text-tealDark">Equipo</span>
             </h2>
-            <div className="relative p-6 md:text-lg rounded-xl text-center border-2 border-gray-300 my-8 max-w-xl">
+            <div className="relative p-6 md:text-lg rounded-xl text-center border-2 border-grayLight my-8 max-w-xl">
               <img className="absolute -top-4 left-6 w-10 bg-orange-100" src={Quotes} alt="Quotes" />
-              <p className="italic text-gray-600">
+              <p className="italic text-grayDark">
                 Está conformado por un médico veterinario y
                 una excelente estilista canina trabajando para usted.
               </p>
@@ -82,15 +82,15 @@ function About2() {
         <div className="flex flex-col items-center md:flex-row md:-ml-96">
           <img className="w-44 md:w-52" src={HuellaCorazon} alt="Huella" />
           <div>
-            <span className="font-shadows text-teal-600 font-bold md:mb-5 text-3xl md:text-4xl xl:text-5xl">No usamos</span>
+            <span className="font-shadows text-tealDark font-bold md:mb-5 text-3xl md:text-4xl xl:text-5xl">No usamos</span>
             {' '}
-            <h2 className="font-bold md:mb-5 text-3xl md:text-4xl xl:text-5xl text-gray-800">
+            <h2 className="font-bold md:mb-5 text-3xl md:text-4xl xl:text-5xl text-grayDarker">
               bozal ni sedación.
 
             </h2>
-            <div className="relative p-6 md:text-lg rounded-xl text-center border-2 border-gray-300 my-8">
+            <div className="relative p-6 md:text-lg rounded-xl text-center border-2 border-grayLight my-8">
               <img className="absolute -top-4 left-6 w-10 bg-orange-100" src={Quotes} alt="Quotes" />
-              <p className="italic text-gray-600">
+              <p className="italic text-grayDark">
                 La idea principal es que tu mascota
                 se sienta cómoda y segura.
               </p>
