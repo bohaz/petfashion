c4956e5d88d1337af90ea613ab02e4c3ea336186
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-27 10:02:16 -0400

    Remove component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Box } from '@mui/material';
 import ContactPage from './ContactPage';
 import Visitanos from '../components/Visitanos';
-/* import TrustmaryWidget from '../components/TrustmaryWidget'; */
 import Instagram from '../components/Instagram';
 import About2 from './About2';
 import Servicios from './Servicios';
@@ -14,7 +13,6 @@ function HomePage() {
       <About2 />
       <Servicios />
       <Instagram />
-      {/* <TrustmaryWidget /> */}
       <Visitanos />
       <ContactPage />
 
