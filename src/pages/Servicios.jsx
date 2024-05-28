fa6df07ef3800bf333fa464201945f13ae61f5f8
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-05-28 11:45:30 -0400

    Remove ring color

--- src/pages/Servicios.jsx
+++ src/pages/Servicios.jsx
@@ -42,7 +42,7 @@ function Servicios() {
             <img className="w-28" src={Card1} alt="icono" />
             <img className="w-10 animate-bounce m-auto mt-12" src={Card5} alt="icono" />
             <button
-              className="text-grayDarker bg-white border-2 border-grayDark hover:outline hover:outline-grayDark outline-offset-4 font-semibold py-2 px-4 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-opacity-50 transition duration-300"
+              className="text-grayDarker bg-white border-2 border-grayDark hover:outline hover:outline-grayDark outline-offset-4 font-semibold py-2 px-4 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-opacity-50 transition duration-300"
               type="button"
               onClick={() => handleNavigate('/CorteYBano')}
             >
@@ -61,7 +61,7 @@ function Servicios() {
             <img className="w-28" src={Card2} alt="icono" />
             <img className="w-10 animate-bounce m-auto mt-12" src={Card5} alt="icono" />
             <button
-              className="text-grayDarker bg-white border-2 border-grayDark hover:outline hover:outline-grayDark outline-offset-4 font-semibold py-2 px-4 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-opacity-50 transition duration-300"
+              className="text-grayDarker bg-white border-2 border-grayDark hover:outline hover:outline-grayDark outline-offset-4 font-semibold py-2 px-4 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-opacity-50 transition duration-300"
               type="button"
               onClick={() => handleNavigate('/SoloBano')}
             >
@@ -80,7 +80,7 @@ function Servicios() {
             <img className="w-28" src={Card4} alt="icono" />
             <img className="w-10 animate-bounce m-auto mt-12" src={Card5} alt="icono" />
             <button
-              className="text-grayDarker bg-white border-2 border-grayDark hover:outline hover:outline-grayDark outline-offset-4 font-semibold py-2 px-4 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-opacity-50 transition duration-300"
+              className="text-grayDarker bg-white border-2 border-grayDark hover:outline hover:outline-grayDark outline-offset-4 font-semibold py-2 px-4 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-opacity-50 transition duration-300"
               type="button"
               onClick={() => handleNavigate('/Booking')}
             >
