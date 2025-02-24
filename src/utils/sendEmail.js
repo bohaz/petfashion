9962e4e3e526eb44c1076fa30410c6dc899b4c34
Ricardo Martínez <ricardomartinezvet@gmail.com>
2025-02-24 11:54:05 -0300

    Create a new send email component

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,39 @@
+import emailjs from 'emailjs-com';
+import { toast } from 'react-toastify';
+
+const serviceId = 'service_pryz19b';
+const templateId = 'template_ta1tj2g';
+const userId = 'x9kDizl6AGXIURnZS';
+
+const sendEmail = (data, onSuccess) => {
+  const emailParams = {
+    user_name: data.ownerName,
+    pet_name: data.petName,
+    user_email: data.email,
+    phone: data.phone,
+    message: 'Gracias por reservar en nuestra peluquería canina. Estas son nuestras condiciones...',
+  };
+
+  emailjs.send(serviceId, templateId, emailParams, userId)
+    .then(() => {
+      toast.success('Reserva realizada con éxito. Revisa tu correo 📩', {
+        position: 'top-center',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+
+      onSuccess();
+    })
+    .catch((error) => {
+      console.error('Error al enviar correo:', error);
+      toast.error('Error al enviar la reserva. Inténtalo de nuevo ❌', {
+        position: 'top-center',
+        autoClose: 3000,
+      });
+    });
+};
+
+export default sendEmail;
