d3540dd3fc58742d70d5d5b2a961c3b7196e8869
Ricardo Martínez <ricardomartinezvet@gmail.com>
2025-02-24 12:32:48 -0300

    Update the whatsapp message

--- src/utils/sendWhatsApp.js
+++ src/utils/sendWhatsApp.js
@@ -1,11 +1,13 @@
 const sendWhatsAppMessage = (data) => {
   const whatsappNumber = '56945570410';
-  const message = '📅 *Nueva Reserva Pet Fashion* 📅\n\n'
-                  + `👤 *Propietario:* ${data.ownerName}\n`
-                  + `🐶 *Mascota:* ${data.petName}\n`
-                  + `📧 *Correo:* ${data.email}\n`
-                  + `📞 *Teléfono:* ${data.phone}\n\n`
-                  + '💬 Por favor, continúa la conversación con el cliente.';
+  const message = `Hola *${data.ownerName}*,\n\n`
+                  + 'Estos son tus datos para agendar tu cita en *Pet Fashion*:\n\n'
+                  + `*Propietario:* ${data.ownerName}\n`
+                  + `*Mascota:* ${data.petName}\n`
+                  + `*Correo:* ${data.email}\n`
+                  + `*Teléfono:* ${data.phone}\n\n`
+                  + 'Por favor, continúa la conversación con el cliente.'
+                  + 'Gracias por la información. En breve te atenderemos.';
 
   const encodedMessage = encodeURIComponent(message);
   const whatsappLink = `https://wa.me/${whatsappNumber}?text=${encodedMessage}`;
