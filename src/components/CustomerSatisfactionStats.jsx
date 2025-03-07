2b19b55cbddf3df7a93f6a7670be2230c83d108d
Ricardo Martínez <ricardomartinezvet@gmail.com>
2025-03-06 21:35:48 -0300

    Refactor the Component

--- src/components/CustomerSatisfactionStats.jsx
+++ src/components/CustomerSatisfactionStats.jsx
@@ -5,45 +5,51 @@ import walkIcon from '../assets/svg icons/Perro.svg';
 import BookingButton from './BookingButton';
 
 const CustomerSatisfactionStats = () => (
-  <section className="bg-orange text-white px-2 py-14 lg:px-28 lg:py-28">
+  <section className="bg-cream text-white px-2 py-14 lg:px-28 lg:py-28">
     <div className="container mx-auto text-center">
-      <h2 className="text-3xl md:text-5xl font-bold text-white mb-6 lg:mb-14">Estatus de Clientes Satisfechos</h2>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-5">
-        <div className="p-4 border border-white rounded-lg flex gap-3 items-center justify-center">
+      <h2 className="text-3xl md:text-5xl font-bold text-grayDarker mb-6 lg:mb-14">
+        Estatus de
+        {' '}
+        <span className="font-shadows text-teal-600">clientes</span>
+        {' '}
+        Satisfechos
+      </h2>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-3">
+        <div className="p-4 border border-grayLight rounded-lg shadow-md flex gap-3 items-center justify-center">
           <div className="bg-softOrange p-2 rounded-md">
             <img src={clientIcon} alt="Clientes Atendidos" className="w-9 h-9" />
           </div>
-          <div className="text-left">
+          <div className="text-left text-grayDark">
             <p className="text-2xl font-bold">100+</p>
             <p className="text-sm">Clientes Atendidos</p>
           </div>
 
         </div>
-        <div className="p-4 border border-white rounded-lg flex gap-3 items-center justify-center">
+        <div className="p-4 border border-grayLight rounded-lg shadow-md flex gap-3 items-center justify-center">
           <div className="bg-softOrange p-2 rounded-md">
             <img src={animalsIcon} alt="Perritos Atendidos" className="w-9 h-9" />
           </div>
-          <div className="text-left">
+          <div className="text-left text-grayDark">
             <p className="text-2xl font-bold">500+</p>
             <p className="text-sm">Perritos Atendidos</p>
           </div>
 
         </div>
-        <div className="p-4 border border-white rounded-lg flex gap-3 items-center justify-center">
+        <div className="p-4 border border-grayLight rounded-lg shadow-md flex gap-3 items-center justify-center">
           <div className="bg-softOrange p-2 rounded-md">
             <img src={medallaIcon} alt="Años de experiencia" className="w-9 h-9" />
           </div>
-          <div className="text-left">
+          <div className="text-left text-grayDark">
             <p className="text-2xl font-bold">5+</p>
             <p className="text-sm">Años de experiencia</p>
           </div>
 
         </div>
-        <div className="p-4 border border-white rounded-lg flex gap-3 items-center justify-center">
+        <div className="p-4 border border-grayLight rounded-lg shadow-md flex gap-3 items-center justify-center">
           <div className="bg-softOrange p-2 rounded-md">
             <img src={walkIcon} alt="Miles Walked" className="w-9 h-9" />
           </div>
-          <div className="text-left">
+          <div className="text-left text-grayDark">
             <p className="text-2xl font-bold">5/5</p>
             <p className="text-sm">Reseñas</p>
           </div>
@@ -51,14 +57,7 @@ const CustomerSatisfactionStats = () => (
         </div>
       </div>
       <div className="mt-14 lg:mt-10">
-        <BookingButton
-          sx={{
-            backgroundColor: 'white',
-            color: 'black',
-            border: '2px solid white',
-            hoverBackgroundColor: '#f5f5f5',
-          }}
-        />
+        <BookingButton />
       </div>
 
     </div>
